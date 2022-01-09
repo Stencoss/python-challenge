d32567f18f06a35d435a7f78c23c6e462c1c8fe2
Meven <merge.conflictstyle>
2022-01-09 13:33:41 -0600

    Added total and monthly

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,15 +11,26 @@ import os           # Used for pathing
 import csv          # Used to read / write csv files
 import pathlib      # Used for file pathing
 
+# Variables
+month_counter = 0           # Initalize month_counter to 0
+total_over_period = 0       # Initalize total_over_period to 0
+
 
 # File paths for input and output
 input_csv = os.path.join(pathlib.Path(__file__).parent.resolve(), 'Resources', 'budget_data.csv')
 output_file = os.path.join(pathlib.Path(__file__).parent.resolve(), "..", "Analysis")
 
-print("************" + input_csv)
+# print("************" + input_csv)                                     # test File path
 
 # Read file in
-with open(input_csv) as csv_file:
-    csvreader = csv.reader(csv_file, delimiter=",")
-    for line in csvreader:
-        print(line)
+with open(input_csv) as csv_file:                                       # Open csv file
+    csvreader = csv.reader(csv_file, delimiter=",")                     # Put in reader and seperate the comma values
+    next(csvreader)                                                     # Skip the header line
+    for line in csvreader:                                              # Start loop to run through the file
+        # print(line)                                                   # Show data
+        month_counter += 1                                              # Count months which counts lines
+        total_over_period = int(line[1]) + total_over_period            # Find the total profit losses
+
+    # Subtract 1 so it does not incle
+    print(month_counter)
+    print(total_over_period)
