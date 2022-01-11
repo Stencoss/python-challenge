4e989c2058f90d450b3714e5518ee9585d675fce
StevenNimble <stadenoia@gmail.com>
2022-01-11 11:35:48 -0600

    Completed totals and percents

---
+++ PyPoll/main.py
@@ -0,0 +1,49 @@
+# Analyze the data set that includes election data
+# TODO: Total number of votes cast
+# TODO: Percentage of votes each candidate won by
+# TODO: Total number of votes each candidate won
+# TODO: Winner of the election based on popular vote
+# TODO: Print to terminal
+# TODO: Print to text file
+
+import csv              # allows the import and export of csv files
+import pathlib          # Pathing required to read and write files
+import os               # Need to use OS for reading paths
+
+# Paths for CSV and also to write results in
+input_csv = os.path.join(pathlib.Path(__file__).parent.resolve(), 'Resources', 'election_data.csv')       # Read location 
+output_file = os.path.join(pathlib.Path(__file__).parent.resolve(), "Analysis")                         # Write location
+
+# Variables
+vote_total = 0                  # Tally the total number of votes
+k_total, k_per, c_total, c_per, l_total, l_per, o_total, o_per = 0,0,0,0,0,0,0,0
+
+
+# Read CSV file in
+with open(input_csv) as csv_file:                           # Open csv file
+    csvreader = csv.reader(csv_file, delimiter=",")         # Put in reader and seperate the comma values
+    next(csvreader)                                         # Skip the header line
+    for line in csvreader:                                  # Start loop to run through the file
+        vote_total = vote_total +  1                        # Find the vote_total by counting rows
+        if line[2] == "Khan":                               # Find each vote count seperatly
+            k_total = k_total + 1
+        elif line[2] == "Correy":
+            c_total = c_total + 1
+        elif line[2] == "Li":
+            l_total = l_total + 1
+        elif line[2] == "O'Tooley":
+            o_total = o_total + 1
+    
+    # Start to show results
+    print("Election Results")
+    print("--------------------------")
+    print("Total Votes: " + str(vote_total))
+    print("--------------------------")
+    print("Khan: " + str(round(k_total / vote_total *100, 3)) + "% (" + str(k_total) +")")
+    print("Correy: " + str(round(c_total / vote_total *100, 3)) + "% (" + str(c_total) +")")
+    print("Li: " + str(round(l_total / vote_total *100, 3)) + "% (" + str(l_total) +")")
+    print("O'Tooley: " + str(round(o_total / vote_total *100, 3)) + "% (" + str(o_total) +")")
+    print("-------------------------")
+    #FIXME:
+    print("Winner: Khan")
+    print("-------------------------")
